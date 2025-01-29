946b04a3a77540c0246d4619e70948583e1771aa
Linkslittlefriend <mghdamsi@gmail.com>
2025-01-30 01:40:30 +0200

    Create Exoskeleton of Calculator, prepare html, js, and css files

---
+++ calculator.js
@@ -0,0 +1,23 @@
+function Add(a,b){
+    return a + b;
+}
+
+function Subtract(a,b){
+    return a - b;
+}
+
+function Multiply(a,b){
+    return a * b;
+}
+
+function Divide(a,b){
+    return a / b;
+}
+
+function Operate(a,b,op){
+    return Add(a,b);
+}
+
+let first;
+let second;
+let operator;
